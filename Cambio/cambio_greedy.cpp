4b6950a5e617b3ade2f25c46bc0b19b7d28fc785
Martin93K <ck313383@fi365.ort.edu.uy>
2025-12-01 15:30:38 -0300

    problema del cambio con greedy terminado

---
+++ Cambio/cambio_greedy.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+//LÓGICA
+//Me fijo si la moneda de mayor valor es menor al cambio que tengo que dar.
+//Si eso pasa entonces la uso, sino busco la siguiente de mayor valor
+
+int monedas[5] = {1,2,5,10,50};
+
+//supongo que el array de monedas está ordenado de meonr a mayor.
+//Si no lo está, tengo que recorrerlo buscando la moneda con mayor valor.
+int problema_del_cambio_greedy(int* array_monedas, int cambio) {
+    int cantidad_de_monedas = 0;
+    for(int i = 4; i >= 0; i--) {
+        int moneda_act = array_monedas[i];
+        while(moneda_act <= cambio) {
+            cambio -= moneda_act;
+            cantidad_de_monedas++;
+        }
+    }
+    //si el cambio quedó en 0 siginifica que pude llegar al cambio, por lo que retorno la cantidad de monedas, en otro caso retorno -1.
+    return cambio == 0 ? cantidad_de_monedas : -1;  
+}
+
+int main() {
+    int cambio;
+    cout << "Ingrese el cambio: ";
+    cin >> cambio;
+    cout << "Para el cambio de " << cambio << " se necesitan: " << problema_del_cambio_greedy(monedas, cambio) << " monedas." << endl;
+    return 0;
+}
